b0d77223b8441cbab953f800835dd0fb67d362b3
JasmineMcCrayFTCC <mccrayj6217@student.faytechcc.edu>
2026-09-11 13:23:17 -0400

    first python lab
    taking input from user and taking output

---
+++ P1LAB1_McCrayJasmine.py
@@ -0,0 +1,8 @@
+# Jasmine McCray
+# P1LAB1_McCrayJasmine.py
+# This program does this
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello, ", first_name, last_name +"!", "Welcome to CTI-110!")
